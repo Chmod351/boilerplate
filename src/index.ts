4e83035fd5d0c69c70fd7bdd7bc180e59df8e24b
yamiltauil <ytauil@itcrowdarg.com>
2025-01-23 19:33:51 -0300

    move index and fix gitignore

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import express from 'express';
+
+import db from './helpers/dbConfig';
+
+import errorHandler from './helpers/errorHandler';
+
+import middlewares from './middlewares/middleware';
+import routes from './app/routes';
+
+
+export const app = express();
+
+const apiUrl="/api/v1"
+
+
+middlewares(app);
+
+
+app.use(`${apiUrl}`,routes)
+
+
+app.use(errorHandler);
+app.listen(8000, () => {
+	db()
+	console.log('Server is running on port http://localhost:8000/ and socket ws://localhost:3000');
+});
